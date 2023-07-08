316b01496bad9ac4ba0a5ecf8bb4e92b4cac84c3
shokirovw <shokirowv@gmail.com>
2023-07-08 13:06:42 +0500

    Key validation is removed

--- app/revalidate/route.js
+++ app/revalidate/route.js
@@ -2,13 +2,7 @@ import { NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
  
 export async function GET(request) {
-  let key = request.nextUrl.searchParams.get('key');
-  if (key && key == "uzb007") {
-    const path = request.nextUrl.searchParams.get('path') || '/';
-    revalidatePath(path)
-    return NextResponse.json({ revalidated: true, now: Date.now() })
-  } else {
-    return NextResponse.json({ revalidated: false })
-  }
-  
+  const path = request.nextUrl.searchParams.get('path') || '/';
+  revalidatePath(path)
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
